fbe0f0ada02366b890db3fe0fe115b2d9be225a9
Soheil Johar <cocobi7060@wiemei.com>
2023-08-05 09:39:19 +0330

    changes

---
+++ learning_log/urls.py
@@ -0,0 +1,12 @@
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include("users.urls")),
+    path('', include("learning_logs.urls")),
+    
+]
+ 
