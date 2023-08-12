17d0502e63ce72585e9f0b2bc912383aef8ec035
Soheil Johar <cocobi7060@wiemei.com>
2023-08-12 20:57:31 +0330

    changes

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -81,24 +81,33 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'soheilproject',
+        'USER': 'soheilproject_user',
+        'PASSWORD': 'ZZum7y8Pa82PdM9CjDwgAYkTfkA7a8Is',
+        'HOST': 'dpg-cjbilvjbq8nc73d52l2g-a',
+        'PORT': '5432',
     }
 }
-'''
 
 
 # Render PostgreSQL database 
 
-import dj_database_url
-
-DATABASES = {
-
-    'default': dj_database_url.parse('postgres://soheilproject_user:ZZum7y8Pa82PdM9CjDwgAYkTfkA7a8Is@dpg-cjbilvjbq8nc73d52l2g-a.oregon-postgres.render.com/soheilproject')
-}
+#import dj_database_url
+#
+#DATABASES = {
+#
+#    'default': dj_database_url.parse("postgres://soheilproject_user:ZZum7y8Pa82PdM9CjDwgAYkTfkA7a8Is@dpg-cjbilvjbq8nc73d52l2g-a.oregon-postgres.render.com/soheilproject")
+#}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
