2a993d9f70479a5876d4fab94c51e0d92c54fa55
Soheil Johar <cocobi7060@wiemei.com>
2023-08-12 12:24:04 +0330

    changes

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,4 @@ urlpatterns = [
     
 ]
  
+urlpatterns += staticfiles_urlpatterns()
